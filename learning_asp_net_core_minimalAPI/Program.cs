97da05b8de793fe68c6cf9c1d405a03e69d4b19f
Spha Zolakhe <siphamandla@coralite.co.za>
2022-06-03 13:42:20 +0200

    indenting code

--- learning_asp_net_core_minimalAPI/Program.cs
+++ learning_asp_net_core_minimalAPI/Program.cs
@@ -27,37 +27,37 @@ await db.Activities.FindAsync(id)
 
 //Post Activity
 app.MapPost("/todoitems", async (Activity activity, ActivityDbContext db) =>
-{
-db.Activities.Add(activity);
-await db.SaveChangesAsync();
+    {
+    db.Activities.Add(activity);
+    await db.SaveChangesAsync();
 });
 
 //Put Activity
 app.MapPut("/todoitems/{id}", async (int id, Activity inputactivity, ActivityDbContext db) =>
 {
-var activity = await db.Activities.FindAsync(id);
+    var activity = await db.Activities.FindAsync(id);
 
-if (activity is null) return Results.NotFound();
+    if (activity is null) return Results.NotFound();
 
-activity.Name = inputactivity.Name;
-activity.IsComplete = inputactivity.IsComplete;
+    activity.Name = inputactivity.Name;
+    activity.IsComplete = inputactivity.IsComplete;
 
-await db.SaveChangesAsync();
+    await db.SaveChangesAsync();
 
-return Results.NoContent();
+    return Results.NoContent();
 });
 
 //Delete Activity
 app.MapDelete("/todoitems/{id}", async (int id, ActivityDbContext db) =>
 {
-if (await db.Activities.FindAsync(id) is Activity activity)
-{
-db.Activities.Remove(activity);
-await db.SaveChangesAsync();
-return Results.Ok(activity);
-}
+    if (await db.Activities.FindAsync(id) is Activity activity)
+    {
+        db.Activities.Remove(activity);
+        await db.SaveChangesAsync();
+        return Results.Ok(activity);
+    }
 
-return Results.NotFound();
+    return Results.NotFound();
 });
 
 app.Run();
